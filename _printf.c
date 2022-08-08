a070c96e6c5fe93d451503374b2060e3c43a9173
YasinAlhadi <yasin.a.m.elhadi@gmail.com>
2022-08-08 02:01:10 -0700

    header file and function added

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+/**
+ * print_op - function to print op (specifier )
+ * @format: passed string
+ * @print_ar: array of struct op
+ * @list: list of arguments to print
+ *
+ * Return: char
+ */
+int print_op(const char *format, fmt_t *print_ar, va_list list)
+{
+	char a;
+	int count, b, c;
+
+	count = 0;
+	b = 0;
+	a = format[b];
+	while (a != '%')
+	{
+		if (a == '%')
+		{
+			c = 0;
+			b++;
+			a = format[b];
+			while (print_ar[c].type != NULL &&
+					a != *(pirnt_ar[c].type))
+				c++;
+			if (print_ar[c].type != NULL)
+				count += print_ar[c].f(list);
+			else
+			{
+				if (a == '\0')
+					return (-1);
+				if (a != '%')
+					count += _putchar('%');
+				count += _putchar(a);
+			}
+		}
+		else
+			count += _putchar(a);
+		b++;
+		a = format[b];
+	}
+	return (count);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: passed srting
+ *
+ * Return: char
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int a = 0;
+
+	check_t op[] = {
+		{"c", ch}
+		{"s", str},
+		{NULL, NULL}
+	};
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	a = print_op(format, op, list);
+	va_end(list);
+	return (a);
+}
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * struct check - function to check format
+ * @type: format type to print
+ * @f: print function
+ */
+typedef struct check
+{
+	char *type;
+	int (*f)();
+} check_t;
+int _putchar(char c);
+int print_op(const char *format, check_t *print_ar, va_list list);
+int _printf(const char *format, ...);
+#endif
